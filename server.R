867ca824ba2b5ed7f061e3f0b4e4ba0615940eea
Tom Ahern <02tahern@ip078035.uvm.edu>
2022-07-27 14:02:09 -0400

    First draft app

--- server.R
+++ server.R
@@ -3,30 +3,38 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(ggExtra)
+#library(ggpubr)
+
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+#shinyServer(
+function(input, output) {
   
   observeEvent(input$run, {
 
+    # Set random or fixed seed based on user selection
+    seed <- ifelse(input$randseed==1, Sys.time(), 2718)
+    set.seed(seed)
+    
     i.row <- 0
 
       # retrieve parameter values from UI
-      niter  <- input$niter
+      niter  <- as.numeric(input$niter)
       size   <- as.numeric(input$studyn)
-      ip     <- input$ip_set
-      pe     <- input$pe_set
-      r      <- input$r_set
-      se     <- input$se_set
-      sp     <- input$sp_set
+      ip     <- as.numeric(input$ip_set)
+      pe     <- as.numeric(input$pe_set)
+      r      <- as.numeric(input$r_set)
+      se     <- as.numeric(input$se_set)
+      sp     <- as.numeric(input$sp_set)
       
       # Initialize results matrix based on selected 'niter' value
       ifelse(niter==1e4, sim.data <- matrix(NA, nrow=1e4, ncol=27),
              ifelse(niter==1e5, sim.data <- matrix(NA, nrow=1e5, ncol=27),
                            sim.data <- matrix(NA, nrow=1e6, ncol=27)))
       
-      # cut 'em off at 100k attempts to reach 'niter' valid iterations
-      niter.max <- 1e5
+      # give up to 2x niter to achieve niter valid records
+      niter.max <- niter*2
             
       # calculate risk in the exposed
       prob   <- min(ip*r,1)
@@ -41,8 +49,8 @@ shinyServer(function(input, output) {
       
       # want up to 'niter' iterations, but stop after trying 'niter.max' times
       while((nsuccess < niter)&(ctr < niter.max)) {
-        
-        # uptick the counter
+    
+      # uptick the counter
         ctr <- ctr+1
         
         # CORRECTLY CLASSIFIED DATA
@@ -123,7 +131,6 @@ shinyServer(function(input, output) {
           i.row <- i.row+1
           
           # add valid results to sim.data
-          #result <- matrix(c(se, sp, pe, ip, r, 
           result <- matrix(c(size, niter, se, sp, pe, ip, r, 
                              flag,
                              se_exp_s, se_unexp_s, sp_exp_s, sp_unexp_s, 
@@ -133,122 +140,261 @@ shinyServer(function(input, output) {
                              v_sto, v_det, 
                              obf_sto, obf_det), nrow=1,ncol=27)
           
-          # add valid iteration to sim.data
-          #sim.data[i.row,] <- result
+          sim.data[i.row,] <- result
       }
       
-      # print message if niter.max reached
-      # cat("Scenario: ",parmset," - Iterations: ",ctr,"\n")
-      # flush.console()
-      # if((nsuccess < niter)&(ctr == niter.max)) {
-      #   cat("Maximum iterations reached with ",nsuccess," successes.\n")
-      #   flush.console()
-      # }
     }
     
-    # # remove unfilled rows from sim.data
-    # i.row <- i.row+1
-    # if(i.row <= n.row) {
-    #   sim.data <- sim.data[-(i.row:n.row),]
-    # }
+    # remove unfilled rows from sim.data
+    i.row <- i.row+1
+    if(i.row <= niter) {
+      sim.data <- sim.data[-(i.row:niter),]
+    }
     
     # convert sim.data to a data frame
-    #sim.data.df <- data.frame(sim.data)
+    # add column names to sim.data
+    colnames(sim.data) <- c('size', 'niter', 'se_set', 'sp_set', 'pe_set', 'ip_set', 'r_set',
+                            'flag',
+                            'se1_s', 'se0_s', 'sp1_s', 'sp0_s',
+                            'se1_d', 'se0_d', 'sp1_d', 'sp0_d', 
+                            'delta_sp_s', 'delta_se_s', 'delta_sp_d', 'delta_se_d',
+                            'iprc', 'iprm_s', 'iprm_d',
+                            'v_s', 'v_d',
+                            'obf_s', 'obf_d'
+                            )
     
-    # generate derived variables
-    # sim.data.df <- sim.data.df |> 
-    #   mutate(
-    #     # confidence limits for misclassified risk ratio
-    #     ul_s = exp(log(iprm_s) + 1.96 * sqrt(v_s)),
-    #     ll_s = exp(log(iprm_s) - 1.96 * sqrt(v_s)),
-    #     ul_d = exp(log(iprm_d) + 1.96 * sqrt(v_d)),
-    #     ll_d = exp(log(iprm_d) - 1.96 * sqrt(v_d)),
-    #     
-    #     # log bias factors (base 10)
-    #     logobf_s = log10(obf_s),
-    #     logobf_d = log10(obf_d),
-    #     
-    #     # indicator: observed true risk ratio within interval of misclassified risk ratio
-    #     iprc_in_ci_s = ifelse(ll_s <= iprc & iprc <= ul_s,1,0),
-    #     iprc_in_ci_d = ifelse(ll_d <= iprc & iprc <= ul_d,1,0),
-    #     
-    #     # indicator: set true risk ratio within interval of misclassified risk ratio
-    #     iprt_in_ci_s = ifelse(ll_s <= r_set & r_set <= ul_s,1,0),  
-    #     iprt_in_ci_d = ifelse(ll_d <= r_set & r_set <= ul_d,1,0),
-    #     
-    #     # indicator: misclassified risk ratio greater than set true risk ratio
-    #     iprm_gt_iprt_s = ifelse(iprm_s > r_set,1,0),
-    #     iprm_gt_iprt_d = ifelse(iprm_d > r_set,1,0),
-    #     
-    #     # indicator: misclassified risk ratio greater than observed true risk ratio
-    #     iprm_gt_iprc_s = ifelse(iprm_s > iprc,1,0),
-    #     iprm_gt_iprc_d = ifelse(iprm_d > iprc,1,0),
-    #     
-    #     # indicator: misclassified risk ratio between null and set true risk ratio
-    #     iprm_btw1_iprt_s = ifelse(1 <= iprm_s & iprm_s <= r_set,1,0),
-    #     iprm_btw1_iprt_d = ifelse(1 <= iprm_d & iprm_d <= r_set,1,0),
-    #     
-    #     # indicator: misclassified risk ratio between null and observed true risk ratio
-    #     iprm_btw1_iprc_s = ifelse(1 <= iprm_s & iprm_s <= iprc,1,0),
-    #     iprm_btw1_iprc_d = ifelse(1 <= iprm_d & iprm_d <= iprc,1,0),
-    #     
-    #     # indicator: misclassified risk ratio less than null
-    #     iprm_lt_1_s = ifelse(iprm_s < 1,1,0),
-    #     iprm_lt_1_d = ifelse(iprm_d < 1,1,0),
-    #     
-    #     # define set specificity as difference from 1
-    #     sp_setm1=round(1-sp_set,3), 
-    #     
-    #     # re-scale deltas to unit=thousandths for regression
-    #     delta_se_s_1k=delta_se_s*1000, 
-    #     delta_sp_s_1k=delta_sp_s*1000,
-    #     
-    #     # checksums for mutually exclusive indicator sets
-    #     checksum_s=iprm_lt_1_s+iprm_btw1_iprt_s+iprm_gt_iprt_s,
-    #     checksum_d=iprm_lt_1_d+iprm_btw1_iprt_d+iprm_gt_iprt_d
-    #   ) 
+    sim.data.df <- data.frame(sim.data)
     
-    # Make output for Shiny page
-    output$text <- renderText({result})
-    
-    # # Summarizing portions of the distribution
-    # pct_iprm_lt1_s=100*round(mean(s96$iprm_lt_1_s, na.rm=TRUE), digits=5)
-    # pct_iprm_btw1_iprt_s=100*round(mean(s96$iprm_btw1_iprt_s, na.rm=TRUE), digits=5)
-    # pct_iprm_gt_iprt_s=100*round(mean(s96$iprm_gt_iprt_s, na.rm=TRUE), digits=5)
-    # 
-    # dens <- 
-    #   
-    #   ggplot(data=s96, aes(x=iprm_s)) +
-    #   geom_density() +
-    #   geom_vline(aes(xintercept=mean(r_set)), color="blue", linetype="dashed", size=1) +
-    #   geom_vline(aes(xintercept=exp(mean(log(iprc)))), color="red", linetype="dashed", size=1) +
-    #   geom_vline(aes(xintercept=1), color="grey", size=1)
-    # 
-    # 
-    # # add only text to the graph
-    # dens + annotate("text", x = 5.75, y = c(0.15,0.2,0.25), 
-    #                 label = c(paste0("IPRm<1: ", pct_iprm_gt_iprt_s),
-    #                           paste0("1<= IPRm <=IPRt: ", pct_iprm_btw1_iprt_s), 
-    #                           paste0("IPRm<1: ", pct_iprm_lt1_s)),
-    #                 color="orange", 
-    #                 size=4, fontface="plain",
-    #                 hjust = "left")
-    # 
-    # 
-    # # a data frame with all the annotation info
-    # annotation <- data.frame(
-    #   x = c(5.75),
-    #   y = c(0.15,0.2,0.25),
-    #   label = c(paste0("IPRm<1: ", pct_iprm_gt_iprt_s),
-    #             paste0("1<= IPRm <=IPRt: ", pct_iprm_btw1_iprt_s), 
-    #             paste0("IPRm<1: ", pct_iprm_lt1_s))
-    # )
-    # # Add text box
-    # dens + geom_label(data=annotation, aes(x=x, y=y, label=label),                 
-    #                   color="orange", 
-    #                   size=4, fontface="plain",
-    #                   hjust = "left")
+    records <- nrow(sim.data.df)
+    badrecs <- niter-records
+    output$recs <- renderText({records})
+    output$badrecs <- renderText({badrecs})
 
+        
+    # generate derived variables
+    sim.data.df <- sim.data.df |>
+      mutate(
+        
+        # iprm_s=as.numeric(iprm_s),
+        # v_s=as.numeric(v_s),
+        
+        # confidence limits for misclassified risk ratio
+        ul_s = exp(log(iprm_s) + 1.96 * sqrt(v_s)),
+        ll_s = exp(log(iprm_s) - 1.96 * sqrt(v_s)),
+        ul_d = exp(log(iprm_d) + 1.96 * sqrt(v_d)),
+        ll_d = exp(log(iprm_d) - 1.96 * sqrt(v_d)),
+
+        # log bias factors (base 10)
+        logobf_s = log10(obf_s),
+        logobf_d = log10(obf_d),
+
+        # indicator: observed true risk ratio within interval of misclassified risk ratio
+        iprc_in_ci_s = ifelse(ll_s <= iprc & iprc <= ul_s,1,0),
+        iprc_in_ci_d = ifelse(ll_d <= iprc & iprc <= ul_d,1,0),
+
+        # indicator: set true risk ratio within interval of misclassified risk ratio
+        iprt_in_ci_s = ifelse(ll_s <= r_set & r_set <= ul_s,1,0),
+        iprt_in_ci_d = ifelse(ll_d <= r_set & r_set <= ul_d,1,0),
+
+        # indicator: misclassified risk ratio greater than set true risk ratio
+        iprm_gt_iprt_s = ifelse(iprm_s > r_set,1,0),
+        iprm_gt_iprt_d = ifelse(iprm_d > r_set,1,0),
+
+        # indicator: misclassified risk ratio greater than observed true risk ratio
+        iprm_gt_iprc_s = ifelse(iprm_s > iprc,1,0),
+        iprm_gt_iprc_d = ifelse(iprm_d > iprc,1,0),
+
+        # indicator: misclassified risk ratio between null and set true risk ratio
+        iprm_btw1_iprt_s = ifelse(1 <= iprm_s & iprm_s <= r_set,1,0),
+        iprm_btw1_iprt_d = ifelse(1 <= iprm_d & iprm_d <= r_set,1,0),
+
+        # indicator: misclassified risk ratio between null and observed true risk ratio
+        iprm_btw1_iprc_s = ifelse(1 <= iprm_s & iprm_s <= iprc,1,0),
+        iprm_btw1_iprc_d = ifelse(1 <= iprm_d & iprm_d <= iprc,1,0),
+
+        # indicator: misclassified risk ratio less than null
+        iprm_lt_1_s = ifelse(iprm_s < 1,1,0),
+        iprm_lt_1_d = ifelse(iprm_d < 1,1,0),
+
+        # define set specificity as difference from 1
+        sp_setm1=round(1-sp_set,3),
+
+        # re-scale deltas to unit=thousandths for regression
+        delta_se_s_1k=delta_se_s*1000,
+        delta_sp_s_1k=delta_sp_s*1000,
+
+        # checksums for mutually exclusive indicator sets
+        checksum_s=iprm_lt_1_s+iprm_btw1_iprt_s+iprm_gt_iprt_s,
+        checksum_d=iprm_lt_1_d+iprm_btw1_iprt_d+iprm_gt_iprt_d
+      )
+    
+    
+    # Summarizing portions of the distribution
+    pct_iprm_lt1_s=format(round(100*mean(sim.data.df$iprm_lt_1_s, na.rm=TRUE), digits=3), nsmall=2)
+    pct_iprm_btw1_iprt_s=format(round(100*mean(sim.data.df$iprm_btw1_iprt_s, na.rm=TRUE), digits=3), nsmall=2)
+    pct_iprm_gt_iprt_s=format(round(100*mean(sim.data.df$iprm_gt_iprt_s, na.rm=TRUE), digits=3), nsmall=2)
+    
+    ###temp$Se <- format(round(temp$se_set, digits=2), nsmall=2)
+    
+### Make simulation data downloadable
+    output$simdat <- renderTable({
+      sim.data.df()
+    })
+    
+    output$downloadData <- downloadHandler(
+      filename = function() {
+        paste("ndmd-sim-", Sys.Date(), ".csv", sep="")
+      },
+      content = function(file) {
+        write.csv(sim.data.df, file)
+      }
+    )
+    
+      
+### Text output
+
+    # summary of simulated true risk ratios
+    iprc_sum <- summary(sim.data.df$iprc, digits=3)
+    output$iprc_sum <- renderPrint(iprc_sum)
+    
+    # summary of misclassified risk ratios
+    iprm_s_sum <- summary(sim.data.df$iprm_s, digits=3)
+    output$iprm_s_sum <- renderPrint(iprm_s_sum)
+    
+    # summary of areas of misclassified risk ratio distribution
+    output$iprm_areas <- 
+      renderPrint(cat(paste("Summary of misclassified risk ratio distribution:", "\n",
+                            "\u2022", pct_iprm_gt_iprt_s,"%","are greater than the true risk ratio", "\n",
+                            "\u2022", pct_iprm_btw1_iprt_s,"%", "are between the null and the true risk ratio", "\n",
+                            "\u2022", pct_iprm_lt1_s,"%", "are less than the null", "\n")
+                      )
+                  )
+    
+    
+### Visual output
+    
+    # scatter plot of Se_exp, Se_unexp with marginal densities
+    sejd <- ggplot(sim.data.df, aes(x=se1_s, y=se0_s)) +
+      geom_point(color="#1f78b4", alpha=1/20) +
+      scale_x_continuous(limits=c(0.5,1.0)) +
+      scale_y_continuous(limits=c(0.5,1.0)) +
+      xlab("Sensitivity (exposed)") +
+      ylab("Sensitivity (unexposed)") +
+      theme_minimal(base_size = 16)
+      
+        output$se_jointdist <- 
+        renderPlot({
+          ggMarginal(sejd, type="density")
+        })
+
+      # scatter plot of Sp_exp, Sp_unexp with marginal densities
+      spjd <- ggplot(sim.data.df, aes(x=sp1_s, y=sp0_s)) +
+        geom_point(color="#fd8d3c", alpha=1/20) +
+        scale_x_continuous(limits=c(0.9,1.0)) +
+        scale_y_continuous(limits=c(0.9,1.0)) +
+        xlab("Specificity (exposed)") +
+        ylab("Specificity (unexposed)") +
+        theme_minimal(base_size = 16)
+      
+          output$sp_jointdist <- 
+            renderPlot({
+              ggMarginal(spjd, type="density")
+            })
+        
+    
+    # density plot of simulated true risk ratios
+    dens_iprc <- 
+      ggplot(data=sim.data.df, aes(x=iprc)) +
+      geom_density() +
+      scale_x_continuous(breaks=c(0.5,1.0,2.0,3.0,4.0,5.0), limits=c(0.5,r+2)) +
+      geom_vline(aes(xintercept=r),
+                 color = "#1f78b4",
+                 linetype = "dashed",
+                 size = 1) +
+      geom_text(aes(x = r + 0.08,
+                    label = "Set true risk ratio",
+                    y = 0.1),
+                color = "#1f78b4",
+                angle = 90,
+                #vjust = "bottom",
+                hjust = "left") +
+      xlab("Simulated true risk ratio") +
+      ylab("Density") +
+      theme_minimal(base_size = 16)
+    
+    output$dens_iprc <- renderPlot({dens_iprc})
+    
+    # density plot of misclassified risk ratios
+      # data frame with annotation info
+      # annotation <- data.frame(
+      #   x = c(r+0.53, r+0.57, r+0.57, r+0.57),
+      #   y = c(1.7,1.5,1.3,1.1),
+      #   label = c(
+      #     paste0("Percent of misclassified risk ratios:"),
+      #     paste0("\u2022", " greater than the true risk ratio = ", pct_iprm_gt_iprt_s,"%"),
+      #     paste0("\u2022", " between the null and the true risk ratio = ", pct_iprm_btw1_iprt_s,"%"),
+      #     paste0("\u2022", " less than the null = ", pct_iprm_lt1_s,"%")
+      #   )
+      #   )
+      
+      dens_iprms <- 
+        ggplot(data=sim.data.df, aes(x=iprm_s)) +
+        geom_density() +
+        scale_x_continuous(breaks=c(0.5,1.0,2.0,3.0,4.0,5.0), limits=c(0.5,r+2)) +
+        geom_vline(aes(xintercept=r),
+                   color = "#1f78b4",
+                   linetype = "dashed",
+                   size = 1) +
+        geom_text(aes(x = r + 0.08,
+                      label = "Set true risk ratio",
+                      y = 0.1),
+                  color = "#1f78b4",
+                  angle = 90,
+                  #vjust = "bottom",
+                  hjust = "left") +
+        geom_vline(aes(xintercept=1), 
+                   color = "grey", 
+                   linetype = "dashed",
+                   size = 1) +
+        geom_text(aes(x = 1.08,
+                      label = "Null",
+                      y = 0.1),
+                  color = "grey",
+                  angle = 90,
+                  hjust = "left") +
+        xlab("Misclassified risk ratio") +
+        ylab("Density") +
+        theme_minimal(base_size = 16)
+        # geom_text(data=annotation, aes(x=x, y=y, label=label),
+        #            color="black",
+        #            size=5, 
+        #            fontface="plain",
+        #            hjust = "left") +
+        
+      
+    output$dens_iprms <- renderPlot({dens_iprms})
+
+    # scatter plot of delta_se, delta_sp with points colored by bias factor
+    deltascat <- ggplot(sim.data.df, aes(x=delta_se_s, y=delta_sp_s)) +
+      geom_point(aes(color=logobf_s), size=3, alpha=0.5) +
+      labs(caption = bquote("log(BF)=log"(RR[misclassified]/RR[true]))) +
+      scale_x_continuous(name=bquote(Delta[Se]~" = "~Se[exposed] - Se[unexposed])) +
+      scale_y_continuous(name=bquote(Delta[Sp]~" = "~Sp[exposed] - Sp[unexposed])) +
+      theme(axis.text.x = element_text(size=30)) +
+      scale_color_gradient2(low="#150254", 
+                            mid="#f7f7f7",
+                            high="#540402",
+                            midpoint=0,
+                            limits=c(min(sim.data.df$logobf_s), max(sim.data.df$logobf_s)),
+                            space="Lab", 
+                            na.value="black", 
+                            guide = "colorbar",
+                            aesthetics = "color",
+                            name=bquote("log(BF)")) +
+      theme_minimal(base_size = 16)
+    
+    output$deltascat <- renderPlot({deltascat})
     })
 
-})
+}
+#)
--- ui.R
+++ ui.R
@@ -2,59 +2,142 @@
 
 library(shiny)
 library(shinythemes)
+library(shinybusy)
 
-shinyUI(fluidPage(theme=shinytheme("superhero"),
+shinyUI(fluidPage(theme=shinytheme("spacelab"),
 
     # Application title
-    titlePanel("NDMD Simulation"),
-
+    titlePanel("Simulation of non-differential misclassification of disease under (near-) perfect specificity",
+               windowTitle = "NDMD Simulation"),
+    br(),
+    
+    # Busy indicator
+    # add_busy_spinner(spin="scaling-squares",
+    #                 position="full-page"
+    #                 ),
+    
+    # Smails busy GIF
+    # add_busy_gif(src = "https://media.giphy.com/media/HQRgg6ks7nkyY/giphy.gif", height = 70, width = 70, position="full-page"),
+    
+    # Call me Al busy GIF
+    # add_busy_gif(src = "https://media.giphy.com/media/10oguaVKJJqPZe/giphy.gif", height = 100, width = 100, position="full-page"),
+    
+    # A-Team busy GIF
+    add_busy_gif(src = "https://media.giphy.com/media/pvwWuq1gdckNy/giphy.gif", height = 100, width = 100, position="full-page"),
+    
+    
     # Sidebar with inputs for simulation parameters
     sidebarLayout(
-        sidebarPanel(
-          h3("Simulation parameters"),
+        sidebarPanel(width=3,
+          h3("Inputs"),
           hr(),
+          radioButtons("randseed",
+                       "Seed type",
+                       choices=list("Random (system clock)"=1,
+                                    "Fixed (reproducible simulations)"=0)),
           radioButtons("niter", 
-                       "Number of iterations:", 
+                       "Number of iterations", 
                        choices=list("10,000"=1e4, 
                                     "100,000"=1e5, 
                                     "1,000,000"=1e6)),
           radioButtons("studyn",
-                      "Study size:",
+                      "Study size",
                       choices=list("10,000"=1e4,
                                    "50,000"=5e4,
                                    "100,000"=1e5,
                                    "500,000"=5e5,
                                    "1,000,000"=1e6)),
           sliderInput("ip_set",
-                      "Risk in the unexposed:",
+                      "Risk in the unexposed",
                       min = 0.01,
                       max = 0.50,
                       value = 0.05),
           sliderInput("pe_set",
-                      "Exposure prevalence:",
+                      "Exposure prevalence",
                       min=0.05,
                       max=0.50,
+                      step=0.05,
                       value=0.10),
-          numericInput("r_set", 
-                       "True risk ratio:",
-                       value=1),
+          sliderInput("r_set", 
+                      "True risk ratio",
+                      min=1.0,
+                      max=5.0,
+                      step=0.1,
+                      value=1.5
+                      ),
           sliderInput("se_set",
-                      "Sensitivity of disease classification:",
+                      "Sensitivity of disease classification",
                       min=0.5,
                       max=1.0,
                       value=0.8),
           sliderInput("sp_set",
-                      "Specificity of disease classification:",
+                      "Specificity of disease classification",
                       min=0.90,
                       max=1.0,
-                      value=1.0),
-          actionButton("run", "Run simulation")
+                      value=0.99),
+          hr(),
+        
+          actionButton("run", "Run simulation", class="btn-block",
+                       icon = icon("thumbs-up", 
+                                   lib="font-awesome")),
+          br(),
+          #br(),
+        
+          conditionalPanel(condition = "input.run > 0",
+            downloadButton("downloadData", "Download simulation dataset", class="btn-block",
+                         icon=icon("file-csv", 
+                                   lib="font-awesome"))),
+          br(),
+          #br(),
+          
+          a(shiny::actionButton(inputId = "email1", 
+                                label = "Contact author", 
+                                class = "btn-block",
+                                icon = icon("envelope", lib = "font-awesome")),
+            href="mailto:02tahern@med.uvm.edu")
         ),
 
-        # Show a plot of the generated distribution
-        mainPanel(h1("Here are your fucking results:"),
-            #plotOutput("distPlot")
-          verbatimTextOutput("text")
+        # Output display
+        mainPanel(
+
+          conditionalPanel(
+          condition = "input.run > 0",
+          h3("Bias parameter distributions"),
+          fluidRow(
+            column(6,
+                   h4("Sensitivity"),
+                   plotOutput("se_jointdist"),
+                   ),
+            column(6,
+                   h4("Specificity"),
+                   plotOutput("sp_jointdist"),
+                   ),
+            ),
+
+          hr(),
+          
+          h3("Simulation results"),
+          fluidRow(
+            column(6,
+                   h4("Distribution of simulated true risk ratios"),
+                   plotOutput("dens_iprc"),
+                   verbatimTextOutput("iprc_sum"),
+                   ),
+            column(6,
+                   h4("Distribution of misclassified risk ratios"),
+                   plotOutput("dens_iprms"),
+                   verbatimTextOutput("iprm_s_sum"),
+                   verbatimTextOutput("iprm_areas")
+                   )
+            ),
+          fluidRow(
+            column(12,
+                   h4("Bias factor as a function of departure from non-differential sensitivity and specificity (\u0394)"),
+                   plotOutput("deltascat", height="400px")
+                   )
+            )
+          )
         )
     )
-))
+    )
+    )
